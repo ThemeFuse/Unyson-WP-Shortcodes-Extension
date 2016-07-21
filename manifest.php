842b62af384126eb702a34e74123a5cb1d099ec4
moldcraft <moldcraft@email.com>
2016-07-21 14:57:29 +0300

    removed useless requirement
    updated manifest version

--- manifest.php
+++ manifest.php
@@ -8,18 +8,11 @@ $manifest = array(
 		'Lets you insert Unyson shortcodes inside any WordPress editor.',
 		'fw'
 	),
-	'version' => '0.0.9',
+	'version' => '1.0.0',
 	'display' => true,
 	'standalone' => true,
 
 	'requirements' => array(
-		'framework' => array(
-			/**
-			* In that version was solved the bug with children extension requirements when activate an extension
-			*/
-			'min_version' => '2.1.18',
-		),
-
 		'extensions' => array(
 			'shortcodes' => array(
 				'min_version' => '1.3.17'
