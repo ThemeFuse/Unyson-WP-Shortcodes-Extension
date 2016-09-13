5967d6f6740446f99aee7fe5de41679ed2dcd4d0
moldcraft <moldcraft@email.com>
2016-09-13 12:16:14 +0300

    Version 1.0.3

--- manifest.php
+++ manifest.php
@@ -8,7 +8,7 @@ $manifest = array(
 		'Lets you insert Unyson shortcodes inside any WordPress editor.',
 		'fw'
 	),
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'display' => true,
 	'standalone' => true,
 
